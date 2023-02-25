a7e5ecfd688b35252ead507958886fc08be4e264
Yan Fonseca <yangoular@gmail.com>
2023-02-24 23:20:13 -0300

    Criação do programa, utilizando os métodos da classe Aluno (al)

--- Projeto07/Program.cs
+++ Projeto07/Program.cs
@@ -7,7 +7,22 @@ namespace Projeto07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Aluno al = new Aluno();
+            Console.WriteLine("Insira o nome do aluno;");
+            al.Nome = Console.ReadLine();
+            Console.WriteLine("Insira as três do aluno;");
+            al.N1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            al.N2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            al.N3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Nota final; " + al.NF().ToString("F2", CultureInfo.InvariantCulture));
+            if (al.Aprovado())
+            {
+                Console.WriteLine("Aluno Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado, faltando apenas " + al.NR().ToString("F2", CultureInfo.InvariantCulture) + "Pontos");
+            }
         }
     }
 }
