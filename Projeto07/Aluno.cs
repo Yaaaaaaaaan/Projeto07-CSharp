b85dac839aacff684c9511f9a11c78ee0dce2db1
Yan Fonseca <yangoular@gmail.com>
2023-02-24 23:11:07 -0300

    Remoção de bibliotecas sem uso.

--- Projeto07/Aluno.cs
+++ Projeto07/Aluno.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projeto07
+﻿namespace Projeto07
 {
     internal class Aluno
     {
